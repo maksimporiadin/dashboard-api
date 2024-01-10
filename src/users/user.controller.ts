e67bce702b31d7073000c7df160eafa76b44ff07
Maksim_Poriadin <Maksim_Poriadin@epam.com>
2024-01-10 15:42:14 +0200

    start-0002: create controller

---
+++ src/users/user.controller.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+import { BaseController } from "../common/base.controller";
+import { LoggerService } from "../logger/logger.service";
+
+export class UserController extends BaseController {
+    constructor(logger: LoggerService) {
+        super(logger);
+        this.bindRoutes([
+            { path: '/register', method: 'post', func: this.register },
+            { path: '/login', method: 'post', func: this.login },
+            { path: '/test', method: 'get', func: this.test }
+        ]);
+    }
+
+    login(req: Request, res: Response, next: NextFunction) {
+        this.ok(res, 'login');
+    }
+
+    register(req: Request, res: Response, next: NextFunction) {
+        this.ok(res, 'register');
+    }
+
+    test(req: Request, res: Response, next: NextFunction) {
+        this.ok(res, 'hello!');
+    }
+}
